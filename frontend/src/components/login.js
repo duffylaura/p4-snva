821bc72b64e5524f4731ea4cab15a1381581e926
Coffee Pawz <lauralynneduffy@gmail.com>
2023-04-24 18:00:20 -0400

    Login in styling

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,6 +2,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBInput
+  }
+  from 'mdb-react-ui-kit';
 
 const Login  = (props) => { 
 
@@ -31,23 +39,7 @@ const Login  = (props) => {
         } else {
             console.log("You were unable to be logged in.")
             return false; 
-        }
-
-        // original code works but needed async /await added in 
-        //   axios
-        //   .request(options)
-        //   .then(function async (response) {
-        //     console.log(response.data);
-        //     await (response.data.status); 
-        //     console.log("print response  " + response.data.status); 
-            
-        //     if (response.data.status === 'success') {
-        //         return true; 
-        //     }
-        //   }).catch(function (error) {
-        //     console.error(error);
-        //     return false; 
-        //   });         
+        }     
 
     };
 
@@ -94,34 +86,71 @@ const Login  = (props) => {
 
 
     return (
-        <div>
-            <h1> LOGIN PAGE </h1>
-            <form onSubmit={handleFormSubmit}>
-                <label>
-                    Email:  
-                    <input 
-                    name="email"
-                    type = "email"
-                    placeholder= "Enter Email"
-                    value = {formState.email}
-                    onChange= {handleChange}
-                    />
-                </label>
-                <label>
-                    Password  
-                    <input 
-                    name="password"
-                    type = "password"
-                    placeholder= "Enter Password"
-                    value = {formState.password}
-                    onChange= {handleChange}
-                    />
-                </label>
-                <button> Login </button>
-            </form>
-        </div>
+        <MDBContainer className="my-5 gradient-form">
 
-        );
+        <MDBRow>
+  
+          <MDBCol col='6' className="mb-5">
+            <div className="d-flex flex-column ms-5">
+  
+              <div className="text-center">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                  style={{width: '185px'}} alt="logo" />
+                <h4 className="mt-1 mb-5 pb-1"> LOGIN TO YOUR ACCOUNT</h4>
+              </div>
+
+              <form onSubmit={handleFormSubmit}>
+          
+                    <MDBInput 
+                        wrapperClass='mb-4'
+                        label = "Email"
+                        name = "email"
+                        type = "email"
+                        placeholder = "Enter Email"
+                        value = {formState.email}
+                        onChange = {handleChange}
+                        />
+             
+        
+
+                    <MDBInput 
+                        wrapperClass='mb-4'
+                        label = "Password"
+                        name = "password"
+                        type = "password"
+                        placeholder = "Enter Password"
+                        value = {formState.password}
+                        onChange = {handleChange}
+                    />
+       
+  
+  
+              <div className="text-center pt-1 mb-5 pb-1">
+                <MDBBtn className="mb-4 w-100 gradient-custom-2"> Login </MDBBtn>
+              </div>
+
+              </form>
+
+  
+            </div>
+  
+          </MDBCol>
+  
+          <MDBCol col='6' className="mb-5">
+            <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
+  
+              <div className="text-white px-3 py-4 p-md-5 mx-md-4">
+         
+              </div>
+  
+            </div>
+  
+          </MDBCol>
+  
+        </MDBRow>
+  
+      </MDBContainer>
+      );
     }
 
 export default Login; 
